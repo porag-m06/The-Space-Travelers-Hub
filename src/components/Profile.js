e71a30771f0680038e1c35f33c09c086e76781ad
porag-m06 <porag.m06@gmail.com>
2023-07-14 07:18:30 +0600

    profile logic done

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Profile() {
+  const { rockets } = useSelector((storeState) => storeState.rockets);
+  const { missions } = useSelector((storeState) => storeState.missions);
+
+  const listRockets = rockets.filter((rocket) => rocket.reserved);
+  const listMissions = missions.filter((mission) => mission.reserved);
+
+  console.log('List- R:', listRockets, '\nM:', listMissions);
+
   return (
-    <div>Profile</div>
+    <div>
+      <div className="mission-list">
+        <h2>My Missions</h2>
+        <ul>
+          {
+            listRockets.map((rocket) => <li key={rocket.id}>{rocket.name}</li>)
+          }
+        </ul>
+      </div>
+      <div className="rocket-list">
+        <h2>My Rockets</h2>
+        <ul>
+          {
+            listMissions.map((mission) => <li key={mission.mission_id}>{mission.mission_name}</li>)
+          }
+        </ul>
+      </div>
+    </div>
   );
 }
 
